2eef90cac4e33c49704a69ce3261bae2c999be13
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-14 18:45:29 -0300

    ServicesTableComponent init

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
   <main>
     <AboutComponent />
+    <ServicesTableComponent />
   </main>
 </template>
 
@@ -11,4 +12,5 @@
 import HeaderComponent from './components/HeaderComponent.vue';
 import BannerComponent from './components/BannerComponent.vue';
 import AboutComponent from './components/AboutComponent.vue';
+import ServicesTableComponent from './components/ServicesTableComponent.vue';
 </script>
---
+++ src/components/ServicesTableComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <section>
+      <h2>Serviços</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Serviço 1</td>
+            <td>Descrição do Serviço 1</td>
+            <td>R$ 100,00</td>
+          </tr>
+          <tr>
+            <td>Serviço 2</td>
+            <td>Descrição do Serviço 2</td>
+            <td>R$ 200,00</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
+  <style>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+  </style>
