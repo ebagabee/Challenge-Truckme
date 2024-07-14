e8efd3e0f98b7e73fb6eb3975ab08dd2c7e47f04
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-14 18:43:36 -0300

    AboutComponent init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <HeaderComponent />
   <BannerComponent />
+
+  <main>
+    <AboutComponent />
+  </main>
 </template>
 
 <script setup lang="ts">
 import HeaderComponent from './components/HeaderComponent.vue';
 import BannerComponent from './components/BannerComponent.vue';
+import AboutComponent from './components/AboutComponent.vue';
 </script>
---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+      <h2>Sobre nós</h2>
+      <p>
+        A TruckMe é uma empresa dedicada a fornecer soluções logísticas inovadoras
+        <span v-if="showMore"> Mais informações sobre a empresa...</span>
+      </p>
+      <button @click="toggleShowMore">Leia mais</button>
+    </section>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  
+  const showMore = ref(false);
+  
+  function toggleShowMore() {
+    showMore.value = !showMore.value;
+  }
+  </script>
+  
+  <style>
+  section {
+    padding: 2rem;
+  }
+  button {
+    margin-top: 1rem;
+  }
+  </style>
+  
