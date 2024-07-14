a1ed389bfa1ab62d49fbd5cb7347fe439ed95bc4
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-14 19:54:04 -0300

    banner finalizado

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,26 +1,56 @@
 <template>
-    <div class="banner">
-        <div class="banner-text">
-            <h1>Boas vindas ao Truckme</h1>
+    <section class="banner">
+        <div class="banner-overlay"></div>
+        <div class="banner-content">
+            <h1>BOAS VINDAS AO TRUCKME</h1>
+            <p>Sua solução em transporte de qualidade.</p>
         </div>
-    </div>
+    </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
-<style>
+<style scoped>
 .banner {
-    /* background-image: ; */
+    position: relative;
+    background: url('@/assets/banner-background.png');
+    background-size: contain;
+    background-position: center;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
     height: 300px;
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: center;
+    color: #dbd3d3;
 }
 
-.banner-text {
-    color: white;
-    text-align: center;
-    background-color: rgba(0,0,0, .5);
-    padding: 1rem;
+.banner-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.397);
+}
+
+.banner-content {
+    position: relative;
+    z-index: 1;
+    max-width: 800px;
+    padding: 2rem;
+}
+
+.banner h1 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    font-weight: bold;
+}
+
+.banner p {
+    font-size: 1.8rem;
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
 }
 </style>
