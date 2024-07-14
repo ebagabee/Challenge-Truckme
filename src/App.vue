3d9fada7d5f568663612b57160d39a1f862a26de
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-14 18:41:01 -0300

    Template Banner init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <HeaderComponent />
+  <BannerComponent />
 </template>
 
 <script setup lang="ts">
 import HeaderComponent from './components/HeaderComponent.vue';
+import BannerComponent from './components/BannerComponent.vue';
 </script>
---
+++ src/components/BannerComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="banner">
+        <div class="banner-text">
+            <h1>Boas vindas ao Truckme</h1>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.banner {
+    /* background-image: ; */
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.banner-text {
+    color: white;
+    text-align: center;
+    background-color: rgba(0,0,0, .5);
+    padding: 1rem;
+}
+</style>
