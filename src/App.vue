de86a4c19e485dc0c46eb61005a51aadd5dcbe50
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-14 18:47:25 -0300

    ContactFormComponent init

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   <main>
     <AboutComponent />
     <ServicesTableComponent />
+    <ContactFormComponent />
   </main>
 </template>
 
@@ -13,4 +14,5 @@ import HeaderComponent from './components/HeaderComponent.vue';
 import BannerComponent from './components/BannerComponent.vue';
 import AboutComponent from './components/AboutComponent.vue';
 import ServicesTableComponent from './components/ServicesTableComponent.vue';
+import ContactFormComponent from './components/ContactFormComponent.vue';
 </script>
---
+++ src/components/ContactFormComponent.vue
@@ -0,0 +1,53 @@
+<template>
+    <section>
+      <h2>Contato</h2>
+      <form @submit.prevent="handleSubmit">
+        <div>
+          <label for="name">Nome</label>
+          <input type="text" id="name" v-model="name" required />
+        </div>
+        <div>
+          <label for="email">Email</label>
+          <input type="email" id="email" v-model="email" required />
+        </div>
+        <div>
+          <label for="message">Mensagem</label>
+          <textarea id="message" v-model="message" required></textarea>
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+      <p v-if="submitted">Mensagem enviada com sucesso!</p>
+    </section>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  
+  const name = ref('');
+  const email = ref('');
+  const message = ref('');
+  const submitted = ref(false);
+  
+  function handleSubmit() {
+    if (name.value && email.value && message.value) {
+      submitted.value = true;
+      setTimeout(() => {
+        submitted.value = false;
+        name.value = '';
+        email.value = '';
+        message.value = '';
+      }, 3000);
+    }
+  }
+  </script>
+  
+  <style>
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  form div {
+    margin-bottom: 1rem;
+  }
+  </style>
+  
